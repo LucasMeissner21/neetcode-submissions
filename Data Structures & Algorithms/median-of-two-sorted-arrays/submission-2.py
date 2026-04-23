2fa47cfdcbb79b6b4daa361b5fa1f6bbb5e608ff
LucasMeissner21 <lucas.meissner@hotmail.com>
2026-04-23 16:04:37 -0700

    Add: median-of-two-sorted-arrays - submission-2

---
+++ Data Structures & Algorithms/median-of-two-sorted-arrays/submission-2.py
@@ -0,0 +1,31 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        # Binary search to partition until left partition is correct then get median
+        # from there
+        A, B = nums1, nums2
+        total = len(nums1) + len(nums2) 
+        half = total // 2
+
+        if len(B) < len(A):
+            A, B = B, A
+
+        l = 0
+        r = len(A) - 1
+        while True:
+            i = (l + r) // 2 # A
+            j = half - i - 2 # B
+
+            Aleft = A[i] if i >= 0 else float("-infinity") # If in bounds, stay, otherwise set as lowest or highest possible
+            Aright = A[i + 1] if (i + 1) < len(A) else float("infinity")
+            Bleft = B[j] if j >=0 else float("-infinity")
+            Bright = B[j + 1] if (j + 1) < len(B) else float("infinity")
+
+            if Aleft <= Bright and Bleft <= Aright: # left partition is correct
+                if total % 2: # Odd total
+                    return min(Aright, Bright)
+                else: 
+                    return (max(Aleft, Bleft) + min(Aright, Bright)) / 2
+            elif Aleft > Bright:
+                r = i - 1 # Decrease left partition A
+            else:
+                l = i + 1 # Increase left partition A
