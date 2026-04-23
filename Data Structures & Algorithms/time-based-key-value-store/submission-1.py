d83beb5a1b76a8c876c4c56c4e35cbb04a93d632
LucasMeissner21 <lucas.meissner@hotmail.com>
2026-04-23 15:21:03 -0700

    Add: time-based-key-value-store - submission-1

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-1.py
@@ -0,0 +1,29 @@
+class TimeMap:
+
+    def __init__(self):
+        self.timemap = defaultdict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.timemap[key].append((value, timestamp))
+
+    def get(self, key: str, timestamp: int) -> str:
+        states = self.timemap[key]
+        if states == None:
+            return ""
+        l = 0
+        r = len(states) - 1
+        closest = ""
+
+        while l <= r:
+            mid = (l + r) // 2
+            
+            if states[mid][1] == timestamp:
+                return states[mid][0]
+            
+            if states[mid][1] > timestamp:
+                r = mid - 1
+            else:
+                closest = states[mid][0]
+                l = mid + 1
+        
+        return closest
