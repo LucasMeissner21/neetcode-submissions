7265c017c3b5d16ba22fb32ba53935759e02c7a0
LucasMeissner21 <lucas.meissner@hotmail.com>
2026-04-27 18:47:53 -0700

    Add: generate-parentheses - submission-2

---
+++ Data Structures & Algorithms/generate-parentheses/submission-2.py
@@ -0,0 +1,32 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        # Solution 1: Backtracking through solutions
+        res = []
+        current = ["("]
+        remain = {"(" : n - 1, ")" : n}
+
+        def getResults():
+            # Basecase, add result if length matches
+            if len(current) == 2 * n:
+                res.append("".join(current))
+                return
+
+            # Only add open parenthesis if there are some to add
+            if remain["("] > 0:
+                current.append("(")
+                remain["("] -= 1
+                getResults()
+                current.pop()
+                remain["("] += 1
+            # Only add closed parenthesis if there are more of them than open
+            if remain[")"] > remain["("]:
+                current.append(")")
+                remain[")"] -= 1
+                getResults()
+                current.pop()
+                remain[")"] += 1
+            return
+        
+
+        getResults()
+        return res
